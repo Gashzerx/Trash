994913edc7372f961edec77ebc1dd7933e9815c4
Gashzerx <106402594+Gashzerx@users.noreply.github.com>
2023-06-17 14:53:07 +0100

    Added python project
    The first release. Might make it a fully fledged website

---
+++ new_website.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":star:", layout="wide")
+
+# HEADER SECTION
+st.subheader("Hi, this website is terrible :wave")
+st.title("Pretty Interesting")
+st.write("I am making this as a troll")
+st.write("[Learn more >](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
